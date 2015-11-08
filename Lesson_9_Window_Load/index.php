a398b5809690b49b93efd3a72ae2cd32b540d1c8
Andrew Menning <menning23@gmail.com>
2015-11-08 13:46:04 -0700

    Lesson 9 Window Load

---
+++ Lesson_9_Window_Load/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Example</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<iframe src="../Lesson_5_Inline_vs_External_Scripting/index.php"></iframe>
+	<iframe src="../Lesson_8_Load/index.php"></iframe>
+	<iframe src="../Lesson_2_An_Example/index.php"></iframe>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="ext.js"></script>
+</body>
+</html>
