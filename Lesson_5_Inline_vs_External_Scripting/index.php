252c5c84ad4128e50f77684b4b157f5f25df9aa9
Andrew Menning <menning23@gmail.com>
2015-11-08 13:02:30 -0700

    Lesson 5 Inline vs External Scripting

---
+++ Lesson_5_Inline_vs_External_Scripting/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Example</title>
+</head>
+<body>
+	<p id="paragraph">This is a paragraph</p>
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="hide.js"></script>
+</body>
+</html>
