bb88f7215388d25aecaed66d2e32c06b881c3816
Andrew Menning <menning23@gmail.com>
2015-11-08 13:10:11 -0700

    Lesson 6 Document Ready Event

---
+++ Lesson_6_Document_Ready_Event/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Example</title>
+</head>
+<body>
+	<div id="message" style="display: none;">Welcome to my website</div>
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="init.js"></script>
+</body>
+</html>
