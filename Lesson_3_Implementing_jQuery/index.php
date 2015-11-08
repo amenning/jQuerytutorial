7e5f36b528d89e4ee17bc4b4df05c55d016eb97a
Andrew Menning <menning23@gmail.com>
2015-11-08 12:42:05 -0700

    Lesson 3 Implementing jQuery

---
+++ Lesson_3_Implementing_jQuery/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Example</title>
+</head>
+<body>
+	
+	<script type="text/javascript" src="C:\Users\Andrew\Documents\JQuery\jquery-2.1.4.js"></script>
+</body>
+</html>
