4aa15c5f7b1b3fd68f4c947cd9776ca333e66315
Andrew Menning <menning23@gmail.com>
2015-11-08 12:30:20 -0700

    Lesson 2 An Example

---
+++ Lesson_2_An_Example/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Example</title>
+</head>
+<body>
+	<input type="button" value="Hide" id="toggle_message" />
+	<p id="message">You can see this paragraph</p>
+	<script type="text/javascript" src="C:\Users\Andrew\Documents\JQuery\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="toggle.js"></script>
+</body>
+</html>
