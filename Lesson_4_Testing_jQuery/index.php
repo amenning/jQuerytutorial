168d7aa2856b1127ba2e88f2ae5818a4d328ec29
Andrew Menning <menning23@gmail.com>
2015-11-08 12:49:02 -0700

    Lesson 4 Testing jQuery

---
+++ Lesson_4_Testing_jQuery/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Example</title>
+</head>
+<body>
+	<p onclick="$(this).hide();">This is a paragraph</p>
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+</body>
+</html>
