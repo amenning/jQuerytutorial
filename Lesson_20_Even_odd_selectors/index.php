bf62fa8240d16f76f8c19db22bf38429fad2d828
Andrew Menning <menning23@gmail.com>
2015-11-09 16:58:33 -0700

    Lesson 20 Even odd selectors

---
+++ Lesson_20_Even_odd_selectors/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<table class="table">
+		<tr>
+			<td>First row</td>
+		</tr>
+		<tr>
+			<td>Second row</td>
+		</tr>
+		<tr>
+			<td>Third row</td>
+		</tr>
+		<tr>
+			<td>Fourth row</td>
+		</tr>
+				<tr>
+			<td>First row</td>
+		</tr>
+		<tr>
+			<td>Second row</td>
+		</tr>
+		<tr>
+			<td>Third row</td>
+		</tr>
+		<tr>
+			<td>Fourth row</td>
+		</tr>
+	</table>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
