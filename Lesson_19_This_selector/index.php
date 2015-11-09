ffad1635779df4466e1df1a74fac952c591f5bbc
Andrew Menning <menning23@gmail.com>
2015-11-09 16:41:37 -0700

    Lesson 19 This selector

---
+++ Lesson_19_This_selector/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+</head>
+<body>
+	<input id="button" type="button" value="Click me" />
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
