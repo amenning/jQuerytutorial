a8e269c9b1fbf35c9637f4db4e8c8c3af39806f2
Andrew Menning <menning23@gmail.com>
2015-11-09 15:53:12 -0700

    Lesson 16 Text selector

---
+++ Lesson_16_Text_selector/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+</head>
+<body>
+	<input type="text" />
+	
+	<textarea></textarea>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
