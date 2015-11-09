9aef04beb52cd5bc2e029de0ee2ef0955625019e
Andrew Menning <menning23@gmail.com>
2015-11-09 10:40:06 -0700

    Lesson 13 ID Selector

---
+++ Lesson_13_ID_selector/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+</head>
+<body>
+	<input id="name" type="text" value="Hello"/>
+	<p id="text">text</p>
+	<input id="click_me" type="button" value="Click me" />
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
---
+++ Lesson_13_ID_selector/selectors.js
@@ -0,0 +1,7 @@
+var name = $('#name').val();
+alert(name);
+var text = $('#text').text();
+alert(text);
+$('#click_me').click(function(){
+	alert('Hello again');
+});
