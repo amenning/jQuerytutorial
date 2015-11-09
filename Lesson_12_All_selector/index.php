f9af0101af82f82f848bbc59a067b9b5dd9b6b1a
Andrew Menning <menning23@gmail.com>
2015-11-09 10:29:50 -0700

    Lesson 12 All Selector

---
+++ Lesson_12_All_selector/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+</head>
+<body>
+	<div id="area">
+		<p>Paragraph</p>
+		<input type="button" value="Button" />
+		<span>Useless text</span>
+	</div>
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
