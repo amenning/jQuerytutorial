aa2565dfe199b8383cc5f18e0c013124b8c21e05
Andrew Menning <menning23@gmail.com>
2015-11-10 09:57:08 -0700

    Lesson 21 Attribute selectors

---
+++ Lesson_21_Attribute_selectors/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<p>Name:<br /><input type="text"></p>
+	<p>Email:<br /><input type="email" size="30" /></p>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
---
+++ Lesson_21_Attribute_selectors/selectors.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	var email_default = 'Enter your email address...';
+	
+	$('input[type="email"]').attr('value', email_default);
+});
