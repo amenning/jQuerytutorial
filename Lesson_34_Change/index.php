e4156449404c45ed5d5040951b92f57f8c68b744
Andrew Menning <menning23@gmail.com>
2015-11-11 09:09:26 -0700

    Lesson 34 Change

---
+++ Lesson_34_Change/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<select id="list">
+		<option value="one">One</option>
+		<option value="two">Two</option>
+		<option value="three">Three</option>
+	</select>
+	<span id="list_feedback"></span>
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
