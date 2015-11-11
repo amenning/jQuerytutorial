981a18daeabe34958a7bd63515d69b4cbfd0fb07
Andrew Menning <menning23@gmail.com>
2015-11-11 08:53:21 -0700

    Lesson 32 Double Click

---
+++ Lesson_32_Double_Click/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<a href="#" id="menu_link">Menu</a>
+	<div id="menu" style="display:none;">
+		<a href="#">Programs</a><br />
+		<a href="#">Settings</a><br />
+		<a href="#">Log off</a>
+	</div>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
