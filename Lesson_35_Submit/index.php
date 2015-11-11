110e75ed6f10c1bd4b44381b12ee7a40750bd35c
Andrew Menning <menning23@gmail.com>
2015-11-11 09:24:01 -0700

    Lesson 35 Submit

---
+++ Lesson_35_Submit/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<form action="#" id="signup_form">
+		<p>Email: <input type="email" id="user_email" /></p>
+		<p><input type="submit" value="Sign up" /></p>
+	</form>
+ 
+	<div id="signup_feedback"></div>
+ 
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
