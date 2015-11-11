66a10bbc3df11172bd50ffb6625aeaf9d69c29d7
Andrew Menning <menning23@gmail.com>
2015-11-11 10:28:11 -0700

    Lesson 38 Scroll

---
+++ Lesson_38_Scroll/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<textarea id="some_text" rows="7" cols="25">ksjafsjdlf;jlsdjal;fklsdajfkl;sdajklf;jdskljfkldahkjfhdsakfysiuyiuyvuiyxzkvhkzjchkvbbmnwbnmsayfiuyviuoyxcuivuhsabfnmw,bnmwbemabfkehfkjaslfsdflfsdjafklsalkfkl;sjalkfjksl;jflkslkdajfsdlkjafl;sjakfsd;lkjfls;ajfls;jfkls;djfkls;ajfkl;sdjfl;sdjaflsjkfl;sjafl;jsf;jsldkfjklsda;jflsdk;jflsdhfjkhdskjlfhsafsdlahfjdfal</textarea>
+	<div id="some_feedback"></div>
+ 
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
