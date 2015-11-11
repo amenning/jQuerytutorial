4725c4e5da779d422229f72d12f2ff5ecf0eb74e
Andrew Menning <menning23@gmail.com>
2015-11-11 10:21:49 -0700

    Lesson 37 Hover

---
+++ Lesson_37_Hover/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<a href="#" id="menu_videos">Videos</a> / 
+	<a href="#" id="menu_code">Code</a> /
+	<a href="#" id="menu_forums">Forums</a> 
+	
+	<div id="menu_feedback"></div>
+ 
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
---
+++ Lesson_37_Hover/selectors.js
@@ -0,0 +1,9 @@
+$('#menu_videos').hover(function(){
+	$('#menu_feedback').html('Free video tutorials');
+});
+$('#menu_code').hover(function(){
+	$('#menu_feedback').html('Free high quality code');
+});
+$('#menu_forums').hover(function(){
+	$('#menu_feedback').html('A help forum');
+});
