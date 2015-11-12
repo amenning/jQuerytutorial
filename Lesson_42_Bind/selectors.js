11472dad2c318dea34b68014ad6d533b9ea83f50
Andrew Menning <menning23@gmail.com>
2015-11-12 08:44:37 -0700

    Lesson 42 Bind

---
+++ Lesson_42_Bind/selectors.js
@@ -0,0 +1,11 @@
+$('a').bind('mouseenter mouseleave',function(){
+	$(this).toggleClass('bold');
+});
+
+/*
+$('a').mouseenter(function(){
+	$(this).addClass('bold');
+}).mouseleave(function(){
+	$(this).removeClass('bold');
+});
+*/
