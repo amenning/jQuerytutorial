4e10d89e5957c5d84cb6a0a792da122443bb8280
Andrew Menning <menning23@gmail.com>
2015-11-12 10:54:39 -0700

    Lesson 51 Hover over description

---
+++ Lesson_51_Hover_over_description/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="hoverdiv"></div>
+	<p><a class="hover" href="http://www.google.com" hovertext="Google, a powerful search engine">Google</a></p>
+	
+	<p><a class="hover" href="http://www.boardgamegeek.com" hovertext="Board game enthusiast hub">Boardgamegeek</a></p>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
