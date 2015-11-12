533bef6de375fcbf42b86ffffa34c4f254358cf7
Andrew Menning <menning23@gmail.com>
2015-11-12 09:18:07 -0700

    Lesson 46 Character counting remaining on textarea

---
+++ Lesson_46_Character_counting_remaining_on_textarea/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<textarea id="textarea" rows="7" cols="25" maxlength="55"></textarea>
+	<div id="textarea_feedback"></div>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
