dd284d0e74858b6b154753f3ac5c6dc41823d545
Andrew Menning <menning23@gmail.com>
2015-11-14 11:17:56 -0500

    Lesson 57 toggleClass

---
+++ Lesson_57_toggleClass/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<p><input type="text" id="input" /></p>
+	<p><span id="sometext">This is some text</span></p>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
---
+++ Lesson_57_toggleClass/selectors.js
@@ -0,0 +1,6 @@
+$('#sometext').click(function(){
+	$('#sometext').toggleClass('highlight bold');
+});
+$('#input').focus(function(){
+	$('#input').toggleClass('highlight bold');
+});
