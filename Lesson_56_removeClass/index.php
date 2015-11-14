a7b1919ef2a21dfa0c781a03daa1b37f204833cb
Andrew Menning <menning23@gmail.com>
2015-11-14 11:12:00 -0500

    Lesson 56 removeClass updated from deprecated toggle

---
+++ Lesson_56_removeClass/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<span id="sometext">This is some text</span>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
