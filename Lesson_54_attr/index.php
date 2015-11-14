927dd68f771d0f60e55302fc611caa989152f80a
Andrew Menning <menning23@gmail.com>
2015-11-13 20:30:53 -0700

    Lesson 54 attr

---
+++ Lesson_54_attr/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<p><a id="link" href="http://www.google.com" target="_blank">A link</a></p>
+	<p id="attr"></p>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
