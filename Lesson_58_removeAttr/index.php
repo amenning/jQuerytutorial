1146dbdff45fca1772896e2810c6f48732321c92
Andrew Menning <menning23@gmail.com>
2015-11-14 11:26:05 -0500

    Lesson 58 remove Attr

---
+++ Lesson_58_removeAttr/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<p>
+		<input type="checkbox" id="agree" />I agree
+	</p>
+	<input type="button" value="Continue" disabled="disabled" id="continue"/>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
---
+++ Lesson_58_removeAttr/selectors.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('#agree').change(function(){
+		state = $(this).val();
+		alert(state);
+		
+	});
+});
