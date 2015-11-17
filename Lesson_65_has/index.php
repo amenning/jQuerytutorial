1faf77aa2bb8976cd60c77428d1204946927cb18
Andrew Menning <menning23@gmail.com>
2015-11-16 20:30:21 -0500

    Lesson 65 has

---
+++ Lesson_65_has/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	Menu 1
+	<ul>
+		<li>Menu item 1</li>
+		<li>Menu item 2</li>
+	</ul>
+	<br/>
+	Menu 2
+	<ul>
+	</ul>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
