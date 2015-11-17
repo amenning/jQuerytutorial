1defac1f0c1698bcf3fd3f1ca808e7cdcf6b6c79
Andrew Menning <menning23@gmail.com>
2015-11-16 20:20:46 -0500

    Lesson 64 find

---
+++ Lesson_64_find/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<p>This is a <strong>paragraph</strong>, some things are <strong>bold</strong> and some are <strong>not</strong>.</p>
+	
+	<strong>Outside of paragraph</strong>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
