30430de571337d8e5de08c4a172c435542de355b
Andrew Menning <menning23@gmail.com>
2015-11-17 19:46:21 -0500

    Lesson 69 Fade Out

---
+++ Lesson_69_Fade_Out/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<p><input type="button" id="show" value="Show" />
+	<input type="button" id="hide" value="Hide" /></p>
+	<img id="image" src="DiceFaceFive.png" alt="Sky" style="display:none;"/>
+	<div id="image_feedback"></div>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
