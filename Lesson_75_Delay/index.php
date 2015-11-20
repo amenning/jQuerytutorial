1233013f5cb1fcf369b73d0f9e83967e22646f8e
Andrew Menning <menning23@gmail.com>
2015-11-20 00:52:27 -0600

    Lesson 75 Delay

---
+++ Lesson_75_Delay/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<p><input type="button" value="Go" id="go" /></p>
+	<p id="para">I'm going to vanish.  I'll be back, did you miss me?</p>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
