a7cee126e78cac4c8cfd9cc797ff1aafc06504f6
Andrew Menning <menning23@gmail.com>
2015-11-22 01:05:08 -0600

    Lesson 77 Gallery Fading Effect

---
+++ Lesson_77_Gallery_Fading_Effect/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<img src="DiceFaceFive.png" class="fadeto" />
+	<img src="DiceFaceFive.png" class="fadeto" />
+	<img src="DiceFaceFive.png" class="fadeto" />
+	<br />
+	<img src="DiceFaceFive.png" class="fadeto" />
+	<img src="DiceFaceFive.png" class="fadeto" />
+	<img src="DiceFaceFive.png" class="fadeto" />
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
---
+++ Lesson_77_Gallery_Fading_Effect/selectors.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.fadeto').css('opacity', '0.4');
+	$('.fadeto').mouseover(function(){
+		$(this).fadeTo(100, 1, function(){
+		
+		});
+		$('.fadeto').not(this).fadeTo(100, 0.4);
+	});
+});
