2fde6b0e074e8c943d8622634fdc8818a49cb419
Andrew Menning <menning23@gmail.com>
2015-11-23 01:33:12 -0500

    Lesson 78 Append

---
+++ Lesson_78_Append/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<input id="name" type="test" /> <input id="button" type="button" value="Submit" />
+	
+	<p id="sentence">Your name is: </p>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
