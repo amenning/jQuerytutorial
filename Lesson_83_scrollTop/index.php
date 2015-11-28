8b044f0db8cf4a5c8d117cb79ce9dd56606954b8
Andrew Menning <menning23@gmail.com>
2015-11-28 00:21:16 -0500

    Lesson 83 scrollTop

---
+++ Lesson_83_scrollTop/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<p><textarea id="textarea">jflaskfjl;asjfklsal;fjksa;ljflk;saflk;shalghsalg;jsdkajklsd;aghl;shagl;asoguewapouiewpu ejals;jfl;sajfkl;sajfkl;sjakfl;sajklfjsdlk;ahgskdlagh;lsaklgjs;dajfkls;dajflksjflaskfjl;asjfklsal;fjksa;ljflk;saflk;shalghsalg;jsdkajklsd;aghl;shagl;asoguewapouiewpu ejals;jfl;sajfkl;sajfkl;sjakfl;sajklfjsdlk;ahgskdlagh;lsaklgjs;dajfkls;dajflksjflaskfjl;asjfklsal;fjksa;ljflk;saflk;shalghsalg;jsdkajklsd;aghl;shagl;asoguewapouiewpu ejals;jfl;sajfkl;sajfkl;sjakfl;sajklfjsdlk;ahgskdlagh;lsaklgjs;dajfkls;dajflksjflaskfjl;asjfklsal;fjksa;ljflk;saflk;shalghsalg;jsdkajklsd;aghl;shagl;asoguewapouiewpu ejals;jfl;sajfkl;sajfkl;sjakfl;sajklfjsdlk;ahgskdlagh;lsaklgjs;dajfkls;dajflksjflaskfjl;asjfklsal;fjksa;ljflk;saflk;shalghsalg;jsdkajklsd;aghl;shagl;asoguewapouiewpu ejals;jfl;sajfkl;sajfkl;sjakfl;sajklfjsdlk;ahgskdlagh;lsaklgjs;dajfkls;dajflksjflaskfjl;asjfklsal;fjksa;ljflk;saflk;shalghsalg;jsdkajklsd;aghl;shagl;asoguewapouiewpu ejals;jfl;sajfkl;sajfkl;sjakfl;sajklfjsdlk;ahgskdlagh;lsaklgjs;dajfkls;dajflksjflaskfjl;asjfklsal;fjksa;ljflk;saflk;shalghsalg;jsdkajklsd;aghl;shagl;asoguewapouiewpu ejals;jfl;sajfkl;sajfkl;sjakfl;sajklfjsdlk;ahgskdlagh;lsaklgjs;dajfkls;dajflksjflaskfjl;asjfklsal;fjksa;ljflk;saflk;shalghsalg;jsdkajklsd;aghl;shagl;asoguewapouiewpu ejals;jfl;sajfkl;sajfkl;sjakfl;sajklfjsdlk;ahgskdlagh;lsaklgjs;dajfkls;dajflksjflaskfjl;asjfklsal;fjksa;ljflk;saflk;shalghsalg;jsdkajklsd;aghl;shagl;asoguewapouiewpu ejals;jfl;sajfkl;sajfkl;sjakfl;sajklfjsdlk;ahgskdlagh;lsaklgjs;dajfkls;dajflksjflaskfjl;asjfklsal;fjksa;ljflk;saflk;shalghsalg;jsdkajklsd;aghl;shagl;asoguewapouiewpu ejals;jfl;sajfkl;sajfkl;sjakfl;sajklfjsdlk;ahgskdlagh;lsaklgjs;dajfkls;dajflksjflaskfjl;asjfklsal;fjksa;ljflk;saflk;shalghsalg;jsdkajklsd;aghl;shagl;asoguewapouiewpu ejals;jfl;sajfkl;sajfkl;sjakfl;sajklfjsdlk;ahgskdlagh;lsaklgjs;dajfkls;dajflks</textarea></p>
+	<div id="feedback"></div>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
