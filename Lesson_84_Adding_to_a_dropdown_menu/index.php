29cbf7f479e0365a214947f4c55dcac16504e890
Andrew Menning <menning23@gmail.com>
2015-11-29 22:07:35 -0500

    Lesson 84 Adding to a Dropdown Menu

---
+++ Lesson_84_Adding_to_a_dropdown_menu/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<p><a href="#" class="link">Bread</a> <a href="#" class="link">Sugar</a> <a href="#" class="link">Tea</a> </p>
+	
+	<select id="list">
+		
+	</select>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
