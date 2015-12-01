8f85379412ef3ece14aed7e47568120de1a808ba
Andrew Menning <menning23@gmail.com>
2015-11-30 22:07:30 -0500

    Lesson 85 Placing DIV in very center of window

---
+++ Lesson_85_Placng_DIV_in_very_center_of_window/selectors.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	function move_div(){
+		window_width = $(window).width();
+		window_height = $(window).height();
+		alert ('Width: '+window_width+' Height: '+window_height);
+	}
+	
+	move_div();
+	
+});
