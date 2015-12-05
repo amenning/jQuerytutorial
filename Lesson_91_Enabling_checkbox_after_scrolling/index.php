90ab5f9933e50768833c57e3fa2ddb724b02faf6
Andrew Menning <menning23@gmail.com>
2015-12-04 19:45:13 -0500

    Lesson 91 Enabling checkbox after scrolling

---
+++ Lesson_91_Enabling_checkbox_after_scrolling/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+    <p><textarea id="terms">asjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fjl; s ;asjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fjl; s ;asjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fjasjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fjl; s ;asjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fjl; s ;asjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fjasjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fjl; s ;asjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fjl; s ;asjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fjasjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fjl; s ;asjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fjl; s ;asjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fjasjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fjl; s ;asjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fjl; s ;asjf sajf sajlkf; as;lkf sajlfj salfj ls;f l;sdjflsa lfj asl;f ls;djfla s;fj</textarea></p>
+	<p><input id="agree" type="checkbox" /> I agree to the terms and conditions</p>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
