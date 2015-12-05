73ffb61e957aa3ab2d64b5e1e94dd778ec3abfe4
Andrew Menning <menning23@gmail.com>
2015-12-05 17:54:16 -0500

    Lesson 95 Days until event

---
+++ Lesson_95_Days_until_event/selectors.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	eventTime = Date.parse('20 August 2015')/1000;
+	currentTime = Math.floor($.now()/1000);
+	
+	seconds = eventTime-currentTime;
+	
+	days = Math.floor(seconds/(60*60*24));
+	
+	$('#days').text('Only '+days+' days until the event');
+	
+	
+});
