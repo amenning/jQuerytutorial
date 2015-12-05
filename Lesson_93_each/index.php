62a1e34b966c86fbf555ad58df739cacc92678d5
Andrew Menning <menning23@gmail.com>
2015-12-05 17:33:33 -0500

    Lesson 93 each

---
+++ Lesson_93_each/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <input type="text" id="name" /> <input type="button" value="Insert" id="insert" />
+	
+	<div id="names"></div>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="selectors.js"></script>
+</body>
+</html>
