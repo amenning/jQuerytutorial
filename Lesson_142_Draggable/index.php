adc3c3e571f6b886da414c81e2e78ce9e62f6bb7
Andrew Menning <menning23@gmail.com>
2016-01-19 19:02:21 -0500

    Lesson 142 Draggable

---
+++ Lesson_142_Draggable/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Tutorial</title>
+	<link rel="stylesheet" type="text/css" href="jquery-ui.theme.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	
+	<div><span id="drag">Drag me</span></div>
+	
+	<span id="drag2">Drag me</span><br/><br/><br/>
+	
+	<span id="drag3">Crosshair Drag me</span>
+	
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="..\..\jQuery_src\jquery-ui.js"></script>
+	<script type="text/javascript" src="ui.js"></script>
+</body>
+</html>
---
+++ Lesson_142_Draggable/ui.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$('#drag').draggable({
+		//axis: 'x' // constrain movement only to x-axis
+		containment: 'parent' //'window' or 'document'
+	});
+	
+	var x1 = 50;
+	var y1 = 210;
+	var	x2 = 200;
+	var	y2 = 400;
+	
+	$('#drag2').draggable({
+		containment: [x1, y1, x2, y2]
+	});
+	
+	$('#drag3').draggable({
+		cursor: 'crosshair', //or 'pointer' 
+		opacity: 0.60,
+		grid: [20, 20]
+	});
+});
